2b149a6b12478aeed3a38e2d47b7168c64d6b804
Gustavo Costa <112669156+xgordhacker@users.noreply.github.com>
2022-10-18 13:53:07 -0300

    Urna
    Criado com html, css e js, a função dela é puxar os dados listados em etapas.js
    e reproduzir na urna como se estivesse votando nos candidatos lincados ao número
    deles.

---
+++ Urna Eletronica/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: '22020',
+        nome: 'Jason Mraz',
+        partido: 'ABC',
+        fotos: [
+          {url: '../IMG/vereador11.png', legenda: 'Vereador', small: false}
+        ]
+      },
+      {
+        numero: '70111',
+        nome: 'Russ',
+        partido: 'DEFG',
+        fotos: [
+          {url: '../IMG/vereador12.png', legenda: 'Vereador', small: false}
+        ]
+      }
+    ]
+  },
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: '10',
+        nome: 'EMINEM',
+        partido: 'ABC',
+        vice: 'DR. DRE',
+        fotos: [
+          {url: '../IMG/prefeito11.png', legenda: 'Prefeito', small: false},
+          {url: '../IMG/prefeito12.png', legenda: 'Vice-Prefeito', small: true}
+        ]
+      },
+      {
+        numero: '70',
+        nome: 'Alecia Cara',
+        partido: 'DEFG',
+        vice: 'Bruno Mars',
+        fotos: [
+          {url: '../IMG/prefeito21.png', legenda: 'Prefeito', small: false},
+          {url: '../IMG/prefeito22.png', legenda: 'Vice-Prefeito', small: true}
+        ]
+      }
+    ]
+  }
+]
